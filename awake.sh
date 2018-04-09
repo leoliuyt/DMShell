00c53fbf47f8fbfe1950f61d38e8d2972280912e
leoliu <leoliu@leoliudeMacBook-Pro.local>
2018-04-09 23:07:46 +0800

    commit

---
+++ awake.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sleep 5
+code /Users/leoliu/Documents/Code/LL_Workspace/DMShell/case.sh
+#http://ylq365.iteye.com/blog/1878917
+#https://zhuanlan.zhihu.com/p/25049770
+#http://blog.xiaoniba.com/18602.html
--- sendMail.sh
+++ sendMail.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mail -s "my first email from shell " leoliuyt@sina.cn < ./array.sh
-echo "success" > 1.txt
+mail -s "my first email from shell " leoliuyt@sina.cn < /Users/leoliu/Documents/Code/LL_Workspace//DMShell/array.sh
