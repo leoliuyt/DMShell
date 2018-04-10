03f81dfba85477a05ab3728c0916ade1c44a05d9
lbq <lbq@LEO-MBP.local>
2018-04-10 14:51:35 +0800

    提交

--- awake.sh
+++ awake.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sleep 5
-code /Users/leoliu/Documents/Code/LL_Workspace/DMShell/case.sh
+ls > /Users/lbq/Documents/Code/LL_Workspace/DMShell/1aa.txt
+# code /Users/lbq/Documents/Code/LL_Workspace/DMShell/case.sh
 #http://ylq365.iteye.com/blog/1878917
 #https://zhuanlan.zhihu.com/p/25049770
 #http://blog.xiaoniba.com/18602.html
