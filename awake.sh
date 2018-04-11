ca22e57317c642fe7a2d0c46a38ea274c1b4791f
lbq <lbq@LEO-MBP.local>
2018-04-11 11:14:00 +0800

    修改

--- awake.sh
+++ awake.sh
@@ -6,9 +6,16 @@ then
 say 你好，欢迎登录
 email_date=$(date "+%Y-%m-%d_%H:%M:%S")  
 email_body="周天有人在${email_date} 唤醒了你的电脑"
-email_subject="入侵警告!!!"
+email_from="1033805687@qq.com"  
 email_to="liubaoqiu@meishubao.com"
+email_subject="入侵警告!!!"  
 echo $email_body | mail -s $email_subject $email_to
+# # To和From的顺序不能换 交换后就会有local发送邮件
+# echo "To: ${email_to}" > mail.txt
+# echo "From: ${email_from}" >> mail.txt
+# echo "Subject: ${email_subject}" >> mail.txt
+# echo "${email_body}" >> mail.txt
+# cat mail.txt | /usr/sbin/sendmail -t
 
 if [ $? == 0 ]
 then 
--- sendMail.sh
+++ sendMail.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-mail -s "my first email from shell " leoliuyt@sina.cn < /Users/leoliu/Documents/Code/LL_Workspace//DMShell/array.sh
+
+# 方式一
+# mail -s "my first email from shell " leoliuyt@sina.cn << TAG
+# 邮件内容:
+# 您好
+# TAG
+
+# 方式二
+from="1033805687@qq.com"  
+to="liubaoqiu@meishubao.com"
+#抄送
+cc="leoliuyt@sina.cn"
+
+email_content="您好，这是一封有qq发送到meishubao的邮件"  
+email_subject="测试邮件"  
+
+# To和From的顺序不能换 交换后就会有local发送邮件
+echo "To: ${to}" > mail.txt
+echo "From: ${from}" >> mail.txt
+echo "Cc: ${cc}" >> mail.txt
+echo "Subject: ${email_subject}" >> mail.txt
+echo "${email_content}" >> mail.txt
+cat mail.txt | /usr/sbin/sendmail -t
