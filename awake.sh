011d8aa477983e99fd5d538c5b9d01921f668498
lbq <lbq@LEO-MBP.local>
2018-04-11 09:43:05 +0800

    修改

--- awake.sh
+++ awake.sh
@@ -5,20 +5,20 @@ then
 
 say 你好，欢迎登录
 email_date=$(date "+%Y-%m-%d_%H:%M:%S")  
-email_body="周天有人在${email_date} 登陆了你的电脑"
+email_body="周天有人在${email_date} 唤醒了你的电脑"
 email_subject="入侵警告!!!"
 email_to="liubaoqiu@meishubao.com"
-echo "leoliu1033" | (sudo echo $email_body | mail -s $email_subject $email_to) 
+echo $email_body | mail -s $email_subject $email_to
 
 if [ $? == 0 ]
 then 
 user=$(whoami)
-echo -e "邮件发送成功\n $email_body $email_to $user" > ~/Documents/Code/LL_Workspace/DMShell/1aa.txt
+echo -e "邮件发送成功\n $email_body $email_to $user" > ~/Documents/Code/LL_Workspace/DMShell/log.txt
 # ls > /Users/lbq/Documents/Code/LL_Workspace/DMShell/1aa.txt
 else 
-echo -e "发送失败\n" > ~/Documents/Code/LL_Workspace/DMShell/1aa.txt
+echo -e "发送失败\n" > ~/Documents/Code/LL_Workspace/DMShell/log.txt
 fi
 
 else 
-echo -e "失败\n" > ~/Documents/Code/LL_Workspace/DMShell/1aa.txt
+echo -e "今天不是周天\n" > ~/Documents/Code/LL_Workspace/DMShell/log.txt
 fi
